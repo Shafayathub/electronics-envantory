e6f8eaeb81f93577c1db9a4d5d4d403e0522de85
Shafayathub <shafayat.ms@gamilcom>
2023-01-22 11:27:14 +0600

    first commit

---
+++ src/components/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Banner from './Banner';
+
+const Home = () => {
+  return (
+    <>
+      <main className="max-w-screen-xl mx-auto">
+        <h1>Electronic Envantory Home</h1>
+        <Banner></Banner>
+      </main>
+    </>
+  );
+};
+
+export default Home;

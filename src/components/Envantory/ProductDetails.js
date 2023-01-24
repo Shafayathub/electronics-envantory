688e4914368cadd6d3d3c0d208351be1f36a1ba2
Shafayathub <shafayat.ms@gamilcom>
2023-01-24 15:59:52 +0600

    stock management is ok

--- src/components/Envantory/ProductDetails.js
+++ src/components/Envantory/ProductDetails.js
@@ -31,6 +31,28 @@ const ProductDetails = () => {
       });
     setProduct(delivered);
   };
+  const increaseStock = (event) => {
+    event.preventDefault();
+    const number = event.target.quantity.value;
+    const newQuantity = product.quantity + parseInt(number);
+    const updatedStock = { ...product, quantity: newQuantity };
+    const id = product._id;
+    const url = `http://localhost:5000/product/${id}`;
+    fetch(url, {
+      method: 'PUT',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify(updatedStock),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setProduct(data);
+        toast('Updated successfully');
+      });
+    setProduct(updatedStock);
+    event.target.reset();
+  };
   return (
     <div className="p-5 min-h-screen">
       <div className="card w-80 lg:w-96 glass mx-auto">
@@ -61,6 +83,20 @@ const ProductDetails = () => {
           </div>
         </div>
       </div>
+      <section className="mt-3 flex flex-col md:flex-row gap-3 justify-center items-center">
+        <h5>Increase Quantity</h5>
+        <form onSubmit={increaseStock} className="form-control">
+          <div className="input-group">
+            <input
+              type="number"
+              name="quantity"
+              placeholder="quantity"
+              className="input input-bordered"
+            />
+            <button className="btn btn-square">add</button>
+          </div>
+        </form>
+      </section>
     </div>
   );
 };
